5835f3535f854a2e5f44b59f6e91a085748ff2af
subiram <srisram272007@gmail.com>
2023-12-30 15:41:39 +0530

    added search and sort functionality

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -1,19 +1,60 @@
-import React from 'react';
-import './Stock.css'
+// src/components/StockCard.js
+import React, { useState } from 'react';
+import './Stock.css'; // Import the stock.css file
 
 const StockCard = ({ stocks }) => {
-  if (!stocks || stocks.length === 0) {
-    return <div>No stock data available for table view.</div>;
-  }
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortBy, setSortBy] = useState('');
+
+  const filteredStocks = stocks.filter((stock) =>
+    stock.symbol.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const sortStocks = () => {
+    if (sortBy === 'symbol') {
+      return filteredStocks.sort((a, b) => a.symbol.localeCompare(b.symbol));
+    } else if (sortBy === 'latestPrice') {
+      return filteredStocks.sort((a, b) => a.latestPrice - b.latestPrice);
+    } else {
+      return filteredStocks;
+    }
+  };
+
+  const handleSortChange = (e) => {
+    setSortBy(e.target.value);
+  };
+
   return (
-    <div className="stock-card-container">
-      {stocks.map((stock) => (
-        <div key={stock.symbol} className="stock-card">
-          <h3>{stock.symbol}</h3>
-          <p>{stock.companyName}</p>
-          <p>Latest Price: {stock.latestPrice}</p>
+    <div className="stock-container">
+      <div className="stock-controls">
+        <div className="stock-search">
+          <label htmlFor="search">Search Symbol:</label>
+          <input
+            type="text"
+            id="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </div>
-      ))}
+        <div className="stock-sort">
+          <label htmlFor="sort">Sort By:</label>
+          <select id="sort" value={sortBy} onChange={handleSortChange}>
+            <option value="">None</option>
+            <option value="symbol">Symbol</option>
+            <option value="latestPrice">Latest Price</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="stock-card-container">
+        {sortStocks().map((stock) => (
+          <div key={stock.symbol} className="stock-card">
+            <h3>{stock.symbol}</h3>
+            <p>{stock.companyName}</p>
+            <p>Latest Price: {stock.latestPrice}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/StockTable.js
+++ src/components/StockTable.js
@@ -1,29 +1,70 @@
-import React from 'react';
-import './Stock.css';
+// src/components/StockTable.js
+import React, { useState } from 'react';
+import './Stock.css'; // Import the stock.css file
 
 const StockTable = ({ stocks }) => {
-  if (!stocks || stocks.length === 0) {
-    return <div>No stock data available for table view.</div>;
-  }
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortBy, setSortBy] = useState('');
+
+  const filteredStocks = stocks.filter((stock) =>
+    stock.symbol.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const sortStocks = () => {
+    if (sortBy === 'symbol') {
+      return filteredStocks.sort((a, b) => a.symbol.localeCompare(b.symbol));
+    } else if (sortBy === 'latestPrice') {
+      return filteredStocks.sort((a, b) => a.latestPrice - b.latestPrice);
+    } else {
+      return filteredStocks;
+    }
+  };
+
+  const handleSortChange = (e) => {
+    setSortBy(e.target.value);
+  };
+
   return (
-    <table  className="stock-table">
-      <thead>
-        <tr>
-          <th>Symbol</th>
-          <th>Company Name</th>
-          <th>Latest Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {stocks.map((stock) => (
-          <tr key={stock.symbol}>
-            <td>{stock.symbol}</td>
-            <td>{stock.companyName}</td>
-            <td>{stock.latestPrice}</td>
+    <div className="stock-container">
+      <div className="stock-controls">
+        <div className="stock-search">
+          <label htmlFor="search">Search Symbol:</label>
+          <input
+            type="text"
+            id="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+        <div className="stock-sort">
+          <label htmlFor="sort">Sort By:</label>
+          <select id="sort" value={sortBy} onChange={handleSortChange}>
+            <option value="">None</option>
+            <option value="symbol">Symbol</option>
+            <option value="latestPrice">Latest Price</option>
+          </select>
+        </div>
+      </div>
+
+      <table className="stock-table">
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Company Name</th>
+            <th>Latest Price</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {sortStocks().map((stock) => (
+            <tr key={stock.symbol}>
+              <td>{stock.symbol}</td>
+              <td>{stock.companyName}</td>
+              <td>{stock.latestPrice}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
